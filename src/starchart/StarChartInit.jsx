7ada48c57f7325e871525576a6fddce5d101d800
Guy Livnat <glivnat@gmail.com>
2023-08-22 17:32:56 +0300

    minor bug fixes

--- src/starchart/StarChartInit.jsx
+++ src/starchart/StarChartInit.jsx
@@ -5,39 +5,39 @@ const StarChartInit = ({handleClick}) => {
     const starSize = 3;
     const translateCenter = "translate("+height/2+","+width/2+")"
     return (<svg
-            id="chart"
-            width={width}
-            height={height}
-            // viewBox="0 0 500 500"  //zooms the original image
-            style={{backgroundColor : 'black'}}
-            onClick={handleClick}>
+        id="chart"
+        width={width}
+        height={height}
+        // viewBox="0 0 500 500"  //zooms the original image
+        style={{backgroundColor : 'black'}}
+        onClick={handleClick}>
 
-            <defs>
-                <marker
-                id="star" 
-                orient={"auto"} 
-                markerHeight={starSize*2} 
-                markerWidth={starSize*2} 
-                refX={starSize} 
-                refY={starSize}>
-                    <circle r={starSize} cx={starSize} cy={starSize} fill="white"></circle>
-                </marker>
-            </defs>
+        <defs>
+            <marker
+            id="star" 
+            orient={"auto"} 
+            markerHeight={starSize*2} 
+            markerWidth={starSize*2} 
+            refX={starSize} 
+            refY={starSize}>
+                <circle r={starSize} cx={starSize} cy={starSize} fill="white"></circle>
+            </marker>
+        </defs>
 
-            <g
+        <g
             id="orbits"
             stroke="white"
             transform={translateCenter}>
-            </g>
+        </g>
 
-            <path
+        <path
             id="edge"
             stroke="white"
             style={{fill: "none", strokeWidth: "2"}}
             markerEnd="url(#star)"
             transform={translateCenter}
-            >
-            </path>         
+        >
+        </path>         
     </svg>)
 }
 
