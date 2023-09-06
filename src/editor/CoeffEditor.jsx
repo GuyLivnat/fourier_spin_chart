0152a341129d35daaf63e0ddbbfabcb4744a3383
Guy Livnat <glivnat@gmail.com>
2023-09-06 16:15:25 +0300

    added save to custom made by editor

--- src/editor/CoeffEditor.jsx
+++ src/editor/CoeffEditor.jsx
@@ -4,25 +4,34 @@ import CoeffTable from "./CoeffTable";
 import CoeffAdder from "./CoeffAdder";
 
 
-const CoeffEditor = ({coeff, setTick, tick}) => {
+const CoeffEditor = ({coeff, setTick, tick, saveCoeff, setActiveId}) => {
 
-    const [radius, setRadius] = useState(0);
+    const [radius, setRadius] = useState(20);
     const [angle, setAngle] = useState(0);
 
     const pushCoeff = () => {
-        if(!coeff.current.length) coeff = [0, 0, parseFloat(radius), parseFloat(angle)];
-        else coeff.current = [...coeff.current, parseFloat(radius), parseFloat(angle)];
+        if(!coeff.current.length) coeff.current = [0, 0, parseFloat(radius), parseFloat(angle)];
+        else coeff.current.push(parseFloat(radius), parseFloat(angle));
         setTick(tick+1);
+        setActiveId(null);
     }
+
     const deleteCoeff = (e) => {
         const id = parseInt(e.target.parentElement.parentElement.id);
         coeff.current = [...coeff.current.slice(0, id), ...coeff.current.slice(id+2)];
         setTick(tick+1);
+        setActiveId(null);
     }
+
     const resetCoeff = () => {
         coeff.current = [0,0];
         setTick(tick+1);
+        setActiveId(null);
+    }
 
+    const saveWorkingCoeff = () => {
+        let date = new Date;
+        saveCoeff(coeff.current, date.toLocaleString())
     }
 
     return (<>
@@ -36,9 +45,10 @@ const CoeffEditor = ({coeff, setTick, tick}) => {
                 angle={angle}
                 resetCoeff={resetCoeff}
                 pushCoeff={pushCoeff}
+                save={saveWorkingCoeff}
                 />
             </div>
-            <div style={{boxSizing:"content-box", width:"200px", height:"300px", overflowY:"scroll", overflowX:"clip", textAlign:"center"}}>
+            <div style={{boxSizing:"content-box", width:"200px", height:"270px", overflowY:"scroll", overflowX:"clip", textAlign:"center"}}>
                 <CoeffTable lst={coeff.current} del={deleteCoeff}/>
             </div>
         </div>
