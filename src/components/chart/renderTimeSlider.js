0e580b0b32ef11080f6e4f208bc27046af792bc8
Guy Livnat <glivnat@gmail.com>
2023-10-29 10:46:40 +0200

    time slider style

--- src/components/chart/renderTimeSlider.js
+++ src/components/chart/renderTimeSlider.js
@@ -3,7 +3,11 @@ import * as d3 from "d3";
 const renderTimeSlider = (time) => {
     const percentTime = time * 100
     d3.select('#time-slider-fillbar')
-        .style('background', `linear-gradient(90deg, rgb(191, 194, 240) ${percentTime}%, rgba(191, 194, 240, 0.5) ${percentTime}%)`);
+        .style('background', `linear-gradient(90deg, 
+            rgba(191, 194, 240, 0.5) ${percentTime-100}%, 
+            rgb(191, 194, 240) ${percentTime}%, 
+            rgba(191, 194, 240, 0.5) ${percentTime}%,
+            rgb(191, 194, 240) ${percentTime+100}%)`);
  }
 
 export default renderTimeSlider;
