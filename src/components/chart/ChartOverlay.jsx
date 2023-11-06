34daacb03309187da072594d9463439baf45d0b8
GuyLivnat <glivnat@gmail.com>
2023-11-06 09:38:45 +0200

    labeled component as a placeholder

--- src/components/chart/ChartOverlay.jsx
+++ src/components/chart/ChartOverlay.jsx
@@ -5,7 +5,7 @@ const ChartOverlay = ({playable}) => {
             className={'position-absolute justify-content-center align-items-center' + ((!playable)? ' d-flex' : ' d-none')}
             id='chart-overlay'
             style={{bottom:0, right:0, left:0, top:0, background:"blue"}}>
-                load a path or make your own using the editor
+                (placeholder!) load a path or make your own using the editor
         </div>)
 
 }
