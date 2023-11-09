2b33c491d5a2d707b3120daeab096197c1eda430
Guy Livnat <glivnat@gmail.com>
2023-11-09 12:21:43 +0200

    recenter icon

---
+++ src/assets/icons/RecenterIcon.jsx
@@ -0,0 +1,19 @@
+const RecenterIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+<circle class="cls-1" cx="7.9" cy="7.9" r="7.52"/><line class="cls-1" x1="8.66" y1="7.06" x2="10.74" y2="7.06"/><line class="cls-1" x1="8.66" y1="7.06" x2="8.66" y2="4.97"/><line class="cls-1" x1="8.66" y1="7.06" x2="11.59" y2="4.13"/><line class="cls-1" x1="7.13" y1="8.73" x2="5.05" y2="8.73"/><line class="cls-1" x1="7.13" y1="8.73" x2="7.13" y2="10.82"/><line class="cls-1" x1="7.13" y1="8.73" x2="4.21" y2="11.66"/><line class="cls-1" x1="8.66" y1="8.73" x2="8.66" y2="10.82"/><line class="cls-1" x1="8.66" y1="8.73" x2="10.74" y2="8.73"/><line class="cls-1" x1="8.66" y1="8.73" x2="11.59" y2="11.66"/><line class="cls-1" x1="7.13" y1="7.06" x2="7.13" y2="4.97"/><line class="cls-1" x1="7.13" y1="7.06" x2="5.05" y2="7.06"/><line class="cls-1" x1="7.13" y1="7.06" x2="4.21" y2="4.13"/>
+    </svg>
+  );
+};
+
+export default RecenterIcon;
+
