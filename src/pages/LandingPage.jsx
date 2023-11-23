a8240eb94b0b73c8a2df7358e416741d980a1fad
GuyLivnat <glivnat@gmail.com>
2023-11-23 09:19:21 +0200

    landing page wip

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,26 +1,43 @@
 import { TooltipContext } from "../utilities/TooltipContext";
 import { useContext } from "react";
 import LogoIcon from "../assets/icons/LogoIcon";
+import Button from "../components/general_components/Button";
+import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
   const { tooltipIn, tooltipOut } = useContext(TooltipContext);
 
   return (
     <section className="container-fluid text-center col-md-8 px-auto py-5">
-      <LogoIcon size={60} />
-      <h1
-        className="mb-3"
-        onMouseEnter={tooltipIn}
-        onMouseLeave={tooltipOut}
-        data-tooltip="woo! long words!"
-      >
-        Progressivly rotating wave functions using fourier transforms on
-        geometric space
-      </h1>
-      <p className="lead">
-        Let math calculate which chain of circles recreates a given path...{" "}
-        <br /> or just watch the pretty circles spin.
-      </p>
+      <div>
+        <div className="text-primary">
+          <LogoIcon size={60} />
+        </div>
+        <h1
+          className="mb-3"
+          onMouseEnter={tooltipIn}
+          onMouseLeave={tooltipOut}
+          data-tooltip="woo! long words!"
+        >
+          Progressivly rotating wave functions using fourier transforms on
+          geometric space
+        </h1>
+        <p className="lead">
+          Let math calculate which chain of circles recreates a given path...
+          <br /> or just watch the pretty circles spin.
+        </p>
+      </div>
+      <div className="mt-5 col align-items-stretch gap-3">
+        <Button
+          text="Lets go!"
+          className="btn btn-outline-primary  d-inline-block"
+        />
+        <p className=" d-inline-block mx-3">or</p>
+        <Button
+          text="How does this work?"
+          className="btn btn-outline-primary d-inline-block"
+        />
+      </div>
     </section>
   );
 };
