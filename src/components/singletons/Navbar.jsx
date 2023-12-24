abfe88fc5389fdf5181b070c2c7fdec50b48398a
GuyLivnat <glivnat@gmail.com>
2023-12-24 10:36:49 +0200

    pages?

--- src/components/singletons/Navbar.jsx
+++ src/components/singletons/Navbar.jsx
@@ -6,22 +6,22 @@ const Navbar = () => {
   return (
     <nav className="p-2 d-flex justify-content-center text-center bg-secondary">
       <div className="px-3">
-        <NavLink className="navbar-brand" to="/starchart_fourier_react_d3/">
+        <NavLink className="navbar-brand" to="/">
           <LogoIcon size={25} />
         </NavLink>
       </div>
       <div className="px-3">
-        <NavLink className="nav-link" to="/starchart_fourier_react_d3/chart">
+        <NavLink className="nav-link" to="/chart">
           Chart
         </NavLink>
       </div>
       <div className="px-3">
-        <NavLink className="nav-link" to="/starchart_fourier_react_d3/docs">
+        <NavLink className="nav-link" to="/docs">
           Docs
         </NavLink>
       </div>
       <div className="px-3">
-        <NavLink className="nav-link" to="/starchart_fourier_react_d3/about">
+        <NavLink className="nav-link" to="/about">
           About
         </NavLink>
       </div>
